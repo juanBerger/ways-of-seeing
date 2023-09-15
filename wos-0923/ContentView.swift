c7037a57304c631957da971b59e70e3c8d01b299
juanBerger <juan@duotoneaudio.com>
2023-09-15 14:11:38 -0400

    Initial Commit

---
+++ wos-0923/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wos-0923
+//
+//  Created by Juan Aboites on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wos-0923/wos_0923App.swift
@@ -0,0 +1,17 @@
+//
+//  wos_0923App.swift
+//  wos-0923
+//
+//  Created by Juan Aboites on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct wos_0923App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
